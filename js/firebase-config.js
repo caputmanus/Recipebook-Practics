df9845dea8057baa0c35352b3a499a871e01aad1
Sandy <155328525+caputmanus@users.noreply.github.com>
2026-02-24 20:43:26 +0500

    Update firebase-config.js to remove API keys
    Removed sensitive Firebase configuration details.

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,13 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBcECqBxffKx2carNinZ24N06HqRQbOK3g",
-  authDomain: "recipebook-4c8b5.firebaseapp.com",
-  projectId: "recipebook-4c8b5",
-  storageBucket: "recipebook-4c8b5.firebasestorage.app",
-  messagingSenderId: "100648185989",
-  appId: "1:100648185989:web:06776dcc37b7907784f9ec",
-  measurementId: "G-GNFRYYNQXS"
+  /* апи */
 };
 
 const app = initializeApp(firebaseConfig);
