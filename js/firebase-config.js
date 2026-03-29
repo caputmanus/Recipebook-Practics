ebaae5001096c29fe20099eb16c7551c52183a61
Sandy <155328525+caputmanus@users.noreply.github.com>
2026-03-30 00:04:44 +0500

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  /* апи */
+  /* Вставьте свой АПИ все дела */
 };
 
 const app = initializeApp(firebaseConfig);
